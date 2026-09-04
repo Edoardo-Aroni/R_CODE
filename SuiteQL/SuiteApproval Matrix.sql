3124b31a80fe3f67601ddd90bdb877fc5a0c2976
Edoardo-Aroni <edoardoaroni@gmail.com>
2026-09-04 12:35:38 +0100

    S

--- SuiteQL/SuiteApproval Matrix.sql
+++ SuiteQL/SuiteApproval Matrix.sql
@@ -1,6 +1,11 @@
 SELECT 
 am.id as approval_rule_internal_id,
 BUILTIN.DF(custrecord_sas_am_approvalrule) as approval_rule,
+CASE
+    WHEN BUILTIN.DF(custrecord_sas_am_approvalrule) LIKE '%Expense%' THEN 'Expense Report'
+	WHEN BUILTIN.DF(custrecord_sas_am_approvalrule) LIKE '%Journals%' THEN 'Journal'
+	ELSE 'Purchase Order'
+END as record_name,
 BUILTIN.DF(custrecord_sas_am_approvertype) as approver_type,
 CASE
     WHEN e.entityid LIKE 'EM%' THEN e.entityid || ' ' || e.firstname || ' ' || e.lastname
@@ -12,3 +17,10 @@ FROM CUSTOMRECORD_SAS_APPROVALMATRIX am
 left JOIN employee e
 ON e.id = am.custrecord_sas_am_approver
 WHERE am.isinactive = 'F'
+and CASE
+    WHEN BUILTIN.DF(custrecord_sas_am_approvalrule) LIKE '%Expense%' THEN 'Expense Report'
+	WHEN BUILTIN.DF(custrecord_sas_am_approvalrule) LIKE '%Journals%' THEN 'Journal'
+	ELSE 'Purchase Order'
+END = 'Purchase Order'
+
+--and entityid = 'EM0230'
