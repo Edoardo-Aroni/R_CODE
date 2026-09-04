1cede94722da1e9f8c9ac24a6dda615d89a978c8
Edoardo-Aroni <edoardoaroni@gmail.com>
2026-09-04 11:30:12 +0100

    s

---
+++ SuiteQL/SuiteApproval Matrix.sql
@@ -0,0 +1,14 @@
+SELECT 
+am.id as approval_rule_internal_id,
+BUILTIN.DF(custrecord_sas_am_approvalrule) as approval_rule,
+BUILTIN.DF(custrecord_sas_am_approvertype) as approver_type,
+CASE
+    WHEN e.entityid LIKE 'EM%' THEN e.entityid || ' ' || e.firstname || ' ' || e.lastname
+    ELSE e.entityid
+END as approver,
+am.custrecord_sas_am_minapprovalamount as Min_Approval_Amount,
+am.custrecord_sas_am_maxapprovalamount as Max_Approval_Amount
+FROM CUSTOMRECORD_SAS_APPROVALMATRIX am
+left JOIN employee e
+ON e.id = am.custrecord_sas_am_approver
+WHERE am.isinactive = 'F'
